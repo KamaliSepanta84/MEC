830c4903ad2dd0a4ec74d9ce2d40afbdfdb59ae7
KamaliSepanta84 <reymondpalmer507@gmail.com>
2024-11-10 13:58:40 -0500

    Add files via upload

--- sign_up.py
+++ sign_up.py
@@ -1,2 +1,41 @@
-def sign_up(user, password):
-    pass
+from argon2 import *
+from hashlib import *
+import os
+
+
+
+def encrypt(user_name, password , security_level):
+    time_cost , memory_cost = get_security_level(security_level)
+    salt = custom_salt(user_name)
+    user_hashed_password = hash_password(password , time_cost , memory_cost , salt)
+    return user_hashed_password
+
+
+def hash_password(user_password: str , time_cost: int , memory_cost:int , salt: str) -> str:
+    ph = PasswordHasher(time_cost= time_cost , memory_cost= memory_cost)
+    password = user_password
+    hashed_password = ph.hash(password + salt)
+    return hashed_password
+
+
+def custom_salt(user_name:str) -> str:
+    random_value = os.urandom(16)
+    generated_salt = sha256(user_name.encode() + random_value).hexdigest()
+    return generated_salt
+
+
+
+def get_security_level(security_level: str) -> tuple:
+    if security_level == "Medium":
+        time_cost = 2
+        memory_cost = 1024
+    elif security_level == "High":
+        time_cost = 4
+        memory_cost = 2048
+    elif security_level == "Very High":
+        time_cost = 6
+        memory_cost = 4096
+    return time_cost, memory_cost
+
+
+
